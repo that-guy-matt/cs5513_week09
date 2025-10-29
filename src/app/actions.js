87eea39ae7e4eaa5f58287df92be3c21f7efb35c
Matthew <matteidsen@gmail.com>
2025-10-28 23:12:56 -0500

    yet another attempt at permission denied error

--- src/app/actions.js
+++ src/app/actions.js
@@ -8,15 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
 // Replace the function below
 export async function handleReviewFormSubmission(data) {
-    const { app, currentUser } = await getAuthenticatedAppForUser();
-    const db = getFirestore(app);
+    const { firebaseServerApp, currentUser } = await getAuthenticatedAppForUser();
+    const db = getFirestore(firebaseServerApp);
 
     await addReviewToBook(db, data.get("bookId"), {
         text: data.get("text"),
         rating: data.get("rating"),
 
         // This came from a hidden form field.
-         userId: data.get("userId"),
-        //userId: currentUser.uid,
+        //userId: data.get("userId"),
+        userId: currentUser.uid,
     });
 }
